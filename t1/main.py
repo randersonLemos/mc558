f2fd06618e125e68f684f7f734683dab901372c6
Randerson Lemos <randerson.lemos@gmail.com>
2023-03-18 17:39:37 -0300

    Encontrando sequência gráfica

--- t1/main.py
+++ t1/main.py
@@ -1,38 +1,59 @@
 import copy
 
 class SpecialIntegerSequence:
-    def __init__(self, n, D):
-        self.n = n
-        self.D = D
-        self.idxs = list(range(n))
+    def __init__(self, positions, degrees):
+        self.positions = positions
+        self.degrees = degrees
+        self.n = len(positions)
+
+        self.sortedpositions = sorted(positions, key=lambda x: degrees[x], reverse=True)
+
 
     def subsequence(self):
+        sortedposition = self.sortedpositions[0]
+        d = self.degrees[sortedposition]
         n = self.n
-        D = self.D
+        if ( (n-1-d) >= 0 ) and ( (n-1) > 0 ):
+            sortedpositions = self.sortedpositions[::][1:]
+            degrees = self.degrees[::]
+            for pos in range(d):
+                sortedposition = sortedpositions[pos]
+                degrees[sortedposition] = degrees[sortedposition] - 1
 
-        if n-1 >= 0:
-            n = n - 1
-            d = D[0]
-            if d <= n:
-                D = D[::][1:]
-                for idx in range(d):
-                    D[idx] = D[idx] - 1
-            else:
-                n = -1
-                D = []
+            return SpecialIntegerSequence(sortedpositions, degrees)
+        
 
-        other = copy.copy(self)
-        other.n = n
-        other.D = D
-        return other
+            
+            #n = self.n
+            #D = self.D
+
+        #if n-1 >= 0:
+        #    n = n - 1
+        #    d = D[0]
+        #    if d <= n:
+        #        D = D[::][1:]
+        #        for idx in range(d):
+        #            D[idx] = D[idx] - 1
+        #    else:
+        #        n = -1
+        #        D = []
+
+        #other = copy.copy(self)
+        #other.n = n
+        #other.D = D
+        #return other
 
 
     def __bool__(self):
-        return bool(self.D)
+        return bool(self.positions)
 
 
     def __str__(self):
-        return '{}\n{} ({})'.format( self.idxs.__str__(), self.D.__str__(), self.n )
+        _str_  = '{} - ({}) POSITIONS        \n'.format(self.positions, self.n)
+        _str_ += '{} - ({}) DEGREES          \n'.format([self.degrees[pos] for pos in self.positions], self.n)
+        _str_ += '{} - ({}) SORTED POSITIONS \n'.format(self.sortedpositions, self.n)
+        _str_ += '{} - ({}) SORTED DEGREES   \n'.format([self.degrees[pos] for pos in self.sortedpositions], self.n)
+        return _str_
 
 
     def __repr__(self):
@@ -51,17 +72,34 @@ if __name__ == '__main__':
     n = int( args.file.readline().strip() )
     D = list( map(int, args.file.readline().strip().split(' ') ) )
 
-    sis = SpecialIntegerSequence(n, D)
+    positions = list(range(n))
+    degrees = D
+    sis = SpecialIntegerSequence(positions, degrees)
 
-
-    is_graph_sequence = False
-    while sis:
+    print(sis)
+    for _ in range(n):
+        _sis = sis.subsequence()
+        if not _sis:
+            break
+        sis = _sis
         print(sis)
-        if (sis.n == 1) and (sis.D[0] == 0) : is_graph_sequence = True
-        sis = sis.subsequence()
     
-    if is_graph_sequence:
-        print('Sequencia gráfica')
-    else:
-        print('Não é sequencia gráfica')
+    msg ='NÃO É SEQUÊNCIA GRÁFICA'
+    if sis.n == 1:
+        pos = sis.sortedpositions[0]
+        deg = sis.degrees[pos]
+        if deg == 0:
+            msg = 'SEQUÊNCIA GRÁFICA'
+
+    print(msg)
+    #is_graph_sequence = False
+    #while sis:
+    #    print(sis)
+    #    if (sis.n == 1) and (sis.D[0] == 0) : is_graph_sequence = True
+    #    sis = sis.subsequence()
+    #
+    #if is_graph_sequence:
+    #    print('Sequencia gráfica')
+    #else:
+    #    print('Não é sequencia gráfica')
 
