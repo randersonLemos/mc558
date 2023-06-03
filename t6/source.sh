3c9cf0a08a2c20f910fca149bd5a0c7bb7ff3b71
Randerson Lemos <randerson.lemos@gmail.com>
2023-06-03 19:22:31 -0300

    Reading all files

---
+++ t6/source.sh
@@ -0,0 +1,2 @@
+g++ -o t6 t6.cpp shortest_path.cpp
+./t6
--- t6/t6.cpp
+++ t6/t6.cpp
@@ -2,20 +2,50 @@
 
 using namespace std;
 
+// int main() {
+//     Grafo g(4);
+
+//     g.adicionaArco(0, 1, 2);
+//     g.adicionaArco(0, 2, 1);
+//     g.adicionaArco(1, 3, 3);
+//     g.adicionaArco(2, 3, 6);
+
+//     cout << "Grafo gerado:" << endl;
+//     cout << g;
+
+//     int *dist = g.caminhoMinimo(0, 3);
+//     cout << "Distância mínima entre 0 e 3: " << dist[3] << endl;
+//     free(dist);
+
+//     return 0;
+// }
+
 int main() {
-    Grafo g(4);
+    int n, Q = 0;
 
-    g.adicionaArco(0, 1, 2);
-    g.adicionaArco(0, 2, 1);
-    g.adicionaArco(1, 3, 3);
-    g.adicionaArco(2, 3, 6);
+    int v[150] = { 0 };  
+    int p[150] = { 0 };  
+    int q[150] = { 0 };  
 
-    cout << "Grafo gerado:" << endl;
-    cout << g;
+    for( int i = 0; i < 150; i++)
+    {
+      cout << "(" << i << ") " << v[i] << " " << p[i] << " " << q[i] << endl;
+    }
+    cout << "---" << endl;
 
-    int *dist = g.caminhoMinimo(0, 3);
-    cout << "Distância mínima entre 0 e 3: " << dist[3] << endl;
-    free(dist);
+    cin >> n >> Q;
+    for( int i = 0; i < n; i++)
+    {
+      cin >> v[i] >> p[i] >> q[i];
+    }
 
-    return 0;
+
+    cout << n << " " << Q << endl;
+    for( int i = 0; i < n; i++)
+    {
+      cout << v[i] << " " << p[i] << " " << q[i] << endl;
+    }
+
+
+
 }
