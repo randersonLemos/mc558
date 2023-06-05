f09656aaae114f84883d3353718cdd213588eb7c
Randerson Lemos <randerson.lemos@gmail.com>
2023-06-05 11:00:38 -0300

    ideia is fine

--- t6/t6.cpp
+++ t6/t6.cpp
@@ -1,27 +1,12 @@
 #include <vector>
 #include "shortest_path.hpp"
 
+
 using namespace std;
 
-//int main() {
-//    Grafo g(4);
-//
-//    g.adicionaArco(0, 1, 2);
-//    g.adicionaArco(0, 2, 1);
-//    g.adicionaArco(1, 3, 3);
-//    g.adicionaArco(2, 3, 6);
-//
-//    cout << "Grafo gerado:" << endl;
-//    cout << g;
-//
-//    int *dist = g.caminhoMinimo(0, 3);
-//    cout << "Distância mínima entre 0 e 3: " << dist[3] << endl;
-//    free(dist);
-//
-//    return 0;
-//}
 
-void loadCoins( int& n, int& Q, int& N, vector<int>& vi, vector<int>& vv, vector<int>& vp, vector<int>& vq ) {
+void loadCoins( int& n, int& Q, int& N, vector<int>& vi, vector<int>& vv, vector<int>& vp, vector<int>& vq ) 
+{
     // Source
     vi.push_back( N ); vv.push_back( 0 ); vp.push_back( 0 ); vq.push_back( 1 );
     N += 1;
@@ -69,82 +54,77 @@ int main() {
 
     loadCoins( n, Q, C, vi, vv, vp, vq );
 
-    cout << "C: "<< C << " Q: " << Q << endl;
-    showCoins( C, vi, vv, vp, vq );
-    cout << endl;
-
     Grafo g( C*(Q+1) );
-    cout << "Grafo gerado:" << endl;
-    cout << g;
-    cout << endl;
+
+    //cout << "C: "<< C << " Q: " << Q << endl;
+    //showCoins( C, vi, vv, vp, vq );
+    //cout << endl;
 
     for( int c = 0; c < C ; c++)
     {
-      if( c == (C - 1) )
-        continue;
+      if( c == (C - 1) ) continue;
 
       for( int q = 0; q < Q+1; q++)  
       {
+        int cc0 = c+1; int qq0 = q;           int pp0 = 0;
+        int cc1 = c+1; int qq1 = q + vv[c+1]; int pp1 = vp[c+1];
+        int i   = computeVerticeIndexFromCQ(c,   q,   Q);
+        int ii0 = computeVerticeIndexFromCQ(cc0, qq0, Q);
+        int ii1 = computeVerticeIndexFromCQ(cc1, qq1, Q);
+
         if( c == 0 && q == 0 )
         {
-          int cc0 = c+1; int qq0 = q;           int pp0 = 0;
-          int cc1 = c+1; int qq1 = q + vv[c+1]; int pp1 = vp[c+1];
-          int i   = computeVerticeIndexFromCQ(c,   q,   Q);
-          int ii0 = computeVerticeIndexFromCQ(cc0, qq0, Q);
-          int ii1 = computeVerticeIndexFromCQ(cc1, qq1, Q);
-
-          cout << "| (" << c << "," << q <<  ")" << "->" << pp0 << "->" << "(" << cc0 << "," << qq0 << ") => "; 
-          cout << "(" << i << "," << ii0  << "," << pp0 <<") \t| "; 
+          //cout << "| (" << c << "," << q <<  ")" << "->" << pp0 << "->" << "(" << cc0 << "," << qq0 << ") => "; 
+          //cout << "(" << i << "," << ii0  << "," << pp0 <<") \t| "; 
           g.adicionaArco(i, ii0, pp0);
           if( ( qq1 < Q+1 ))
           {
-            cout << "(" << c << "," << q <<  ")" << "->" << pp1 << "->" << "(" << cc1 << "," << qq1 << ") => " ; 
-            cout << "(" << i << "," << ii1 << "," << pp1 <<")"; 
+            //cout << "(" << c << "," << q <<  ")" << "->" << pp1 << "->" << "(" << cc1 << "," << qq1 << ") => " ; 
+            //cout << "(" << i << "," << ii1 << "," << pp1 <<")"; 
             g.adicionaArco(i, ii1, pp1);
           }
-          cout << endl;
+          //cout << endl;
         }
-        if( ( c > 0 )  && ( c <  (C - 2) ) )
+        //if( ( c > 0 )  && ( c <  (C - 2) ) )
+        if( ( c > 0 )  && ( c <  (C - 1) ) )
         {
-          int cc0 = c+1; int qq0 = q;           int pp0 = 0;
-          int cc1 = c+1; int qq1 = q + vv[c+1]; int pp1 = vp[c+1];
-          int i   = computeVerticeIndexFromCQ(c,   q,   Q);
-          int ii0 = computeVerticeIndexFromCQ(cc0, qq0, Q);
-          int ii1 = computeVerticeIndexFromCQ(cc1, qq1, Q);
-
-          cout << "| (" << c << "," << q <<  ")" << "->" << pp0 << "->" << "(" << cc0 << "," << qq0 << ") => "; 
-          cout << "(" << i << "," << ii0  << "," << pp0 <<") \t| "; 
+          //cout << "| (" << c << "," << q <<  ")" << "->" << pp0 << "->" << "(" << cc0 << "," << qq0 << ") => "; 
+          //cout << "(" << i << "," << ii0  << "," << pp0 <<") \t| "; 
           g.adicionaArco(i, ii0, pp0);
           if( ( qq1 < Q+1 ))
           {
-            cout << "(" << c << "," << q <<  ")" << "->" << pp1 << "->" << "(" << cc1 << "," << qq1 << ") => " ; 
-            cout << "(" << i << "," << ii1 << "," << pp1 <<")"; 
+            //cout << "(" << c << "," << q <<  ")" << "->" << pp1 << "->" << "(" << cc1 << "," << qq1 << ") => " ; 
+            //cout << "(" << i << "," << ii1 << "," << pp1 <<")"; 
             g.adicionaArco(i, ii1, pp1);
           }
-          cout << endl;
-        }
-        if( c == (C - 2) )
-        {
-          int cc = c+1; int qq = 0;
-          int i   = computeVerticeIndexFromCQ(c ,  q, Q);
-          int ii  = computeVerticeIndexFromCQ(cc, qq, Q);
-          int pp  = 0;
-
-          cout << "| (" << c << "," << q <<  ")" << "->" << pp << "->" << "(" << cc << "," << qq << ") => " ; 
-          cout << "(" << i << "," << ii << "," << pp <<")"; 
-          g.adicionaArco(i, ii, pp);
-          cout << endl;
+          //cout << endl;
         }
       }
-      cout << "===" << endl;
+      //cout << "===" << endl;
     }
     
+    //cout << "Grafo gerado:" << endl;
+    //cout << g;
+    //cout << "---" << endl;
+    
+    int source = 0; bool first = true;
+    for( int q = Q; q >=0; q--)
+    {
+      int target = computeVerticeIndexFromCQ(C-1, q, Q);
+      int *dist = g.caminhoMinimo(source, target);
 
-    cout << "Grafo gerado:" << endl;
-    cout << g;
-    cout << "---" << endl;
+      if( (first) && (dist[target] != 2147483647 ))
+      {
+        cout << dist[target] << endl;
+        break;
+      }
 
-
-
+      if( !(first) && dist[target] != 2147483647 )
+      {
+        cout << q << " " << dist[target] << endl;
+        break;
+      }
+      first = false;
+    }
 }
 
